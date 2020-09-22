7b7b0998a77a5c3217d22ad4f28aa94140231a60
dg03 <dhruvitgandhi20@gmail.com>
2020-09-22 16:42:57 +0530

    Initial Commit for hamburger menu

---
+++ index.js
@@ -0,0 +1,103 @@
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    }else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
+const menuBtn1 = document.querySelector('.menu-btn-1');
+let menuOpen1 = false;
+
+menuBtn1.addEventListener('click', () => {
+    if(!menuOpen1) {
+        menuBtn1.classList.add('open');
+        menuOpen1 = true;
+    }else {
+        menuBtn1.classList.remove('open');
+        menuOpen1 = false;
+    }
+});
+
+const menuBtn2 = document.querySelector('.menu-btn-2');
+let menuOpen2 = false;
+
+menuBtn2.addEventListener('click', () => {
+    if(!menuOpen2) {
+        menuBtn2.classList.add('open');
+        menuOpen2 = true;
+    }else {
+        menuBtn2.classList.remove('open');
+        menuOpen2 = false;
+    }
+});
+
+const menuBtn3 = document.querySelector('.menu-btn-3');
+let menuOpen3 = false;
+
+menuBtn3.addEventListener('click', () => {
+    if(!menuOpen3) {
+        menuBtn3.classList.add('open');
+        menuOpen3 = true;
+    }else {
+        menuBtn3.classList.remove('open');
+        menuOpen3 = false;
+    }
+});
+
+const menuBtn4 = document.querySelector('.menu-btn-4');
+let menuOpen4 = false;
+
+menuBtn4.addEventListener('click', () => {
+    if(!menuOpen4) {
+        menuBtn4.classList.add('open');
+        menuOpen4 = true;
+    }else {
+        menuBtn4.classList.remove('open');
+        menuOpen4 = false;
+    }
+});
+
+const menuBtn5 = document.querySelector('.menu-btn-5');
+let menuOpen5 = false;
+
+menuBtn5.addEventListener('click', () => {
+    if(!menuOpen5) {
+        menuBtn5.classList.add('open');
+        menuOpen5 = true;
+    }else {
+        menuBtn5.classList.remove('open');
+        menuOpen5 = false;
+    }
+});
+
+const menuBtn6 = document.querySelector('.menu-btn-6');
+let menuOpen6 = false;
+
+menuBtn6.addEventListener('click', () => {
+    if(!menuOpen6) {
+        menuBtn6.classList.add('open');
+        menuOpen6 = true;
+    }else {
+        menuBtn6.classList.remove('open');
+        menuOpen6 = false;
+    }
+});
+
+const menuBtn7 = document.querySelector('.menu-btn-7');
+let menuOpen7 = false;
+
+menuBtn7.addEventListener('click', () => {
+    if(!menuOpen7) {
+        menuBtn7.classList.add('open');
+        menuOpen7 = true;
+    }else {
+        menuBtn7.classList.remove('open');
+        menuOpen7 = false;
+    }
+});
